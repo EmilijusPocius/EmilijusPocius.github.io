65aaa7ac4db94cf790e588061db3ed31a25c3cc6
EmilijusPocius <emilijuspocius@gmail.com>
2024-11-18 21:35:43 +0200

    Navmenu active script

---
+++ activeClassScript.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname.split("/").pop();
+    const navLinks = document.querySelectorAll("#navmenu a");
+
+    navLinks.forEach(link => {
+        if (link.getAttribute("href") === currentPage) {
+            link.classList.add("active");
+        }
+    });
+});
