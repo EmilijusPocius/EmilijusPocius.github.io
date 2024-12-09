a8e25411a05ea40e2429582be4876402427a72af
EmilijusPocius <emilijuspocius@gmail.com>
2024-12-09 17:19:01 +0200

    Lab12

--- JS/backToTopButton.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    const backToTopButton = document.querySelector('.back-to-top');
-    
-    if (window.scrollY > 100) {
-        backToTopButton.classList.add('show');
-    } else {
-        backToTopButton.classList.remove('show');
-    }
-});
